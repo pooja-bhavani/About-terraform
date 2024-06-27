9083107bfd5e381ffb9f6467b5d8504b041ce8cb
Pooja bhavani <147735975+pooja-bhavani@users.noreply.github.com>
2024-06-27 21:13:26 +0530

    Update main.tf

--- EC2-instance-creation/main.tf
+++ EC2-instance-creation/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
   }
-  resource "aws_instance" "web" {
+resource "aws_instance" "web" {
   ami           = ami-04f8d7ed2f1a54b14
   instance_type = "t2.micro"
   }
