7c777ceffb69d6694921f161800dbe4608597243
Pooja bhavani <147735975+pooja-bhavani@users.noreply.github.com>
2024-06-27 21:15:31 +0530

    Delete EC2-instance-creation/main.tf

--- EC2-instance-creation/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region     = "ap-south-1"
-  }
-resource "aws_instance" "web" {
-  ami           = ami-04f8d7ed2f1a54b14 # specify appropricate ami id
-  instance_type = "t2.micro"
-  }
